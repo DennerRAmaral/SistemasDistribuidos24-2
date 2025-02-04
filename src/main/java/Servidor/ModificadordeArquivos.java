2160d27bc5e1fc0b5c3b8ea5cb8e1af4770acd40
Denner RAmara <115058016+DennerRAmaral@users.noreply.github.com>
2025-02-03 21:28:48 -0300

    adipiscing

--- src/main/java/Servidor/ModificadordeArquivos.java
+++ src/main/java/Servidor/ModificadordeArquivos.java
@@ -5,28 +5,29 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.regex.Pattern;
 
 public class ModificadordeArquivos {
     static void modifyFile(String filePath, String oldString, String newString) throws IOException {
         File fileToBeModified = new File(filePath);
-        String oldContent = "";
+        StringBuilder oldContent = new StringBuilder();
         BufferedReader reader = null;
         reader = new BufferedReader(new FileReader(fileToBeModified));
         FileWriter writer = null;
         writer = new FileWriter(fileToBeModified);
         try {
-
             //Reading all the lines of input text file into oldContent
-            String line = reader.readLine();
+            String line ;
+            line = reader.readLine();
             while (line != null) {
-                oldContent = oldContent + line + System.lineSeparator();
-
+                oldContent.append(line).append("\n");
                 line = reader.readLine();
             }
+            System.out.println(oldContent);
             //Replacing oldString with newString in the oldContent
-            String newContent = oldContent.replaceAll(oldString, newString);
+            String newContent = oldContent.toString().replaceAll(Pattern.quote(oldString), Pattern.quote(newString));
             //Rewriting the input text file with newContent
-
+            System.out.println(newContent);
             writer.write(newContent);
         } catch (IOException e) {
             e.printStackTrace();
